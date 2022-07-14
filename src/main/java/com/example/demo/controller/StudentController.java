a9292802136e8e58dbe5984c9fad2f37c67fdfdd
RyanAlbilihi <78346329+RyanAlbilihi@users.noreply.github.com>
2022-07-14 11:17:41 +0300

    get, create, delete CRUD for in StudentController

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -8,8 +8,6 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import javax.websocket.server.PathParam;
 import java.util.Optional;
 
 @RestController
@@ -22,10 +20,10 @@ public class StudentController {
     @Autowired
     public StudentController(StudentRepository studentRepository) {this.studentRepository = studentRepository;}
 
-    @GetMapping(path = "/get")                   //return all entity in normal way
+    @GetMapping(path = "/getAll")                   //return all entity in normal way
     public ResponseEntity getAllStudents(){return  ResponseEntity.ok(this.studentRepository.findAll());}
 
-    @GetMapping(path = "/getParam/{id}")         //return specific Path by using @PathVariable
+    @GetMapping(path = "/getParam/{id}")         //return specific id entity by using @PathVariable
     public Optional<Student> getStudentByParam(@PathVariable Long id){
         return this.studentRepository.findById(id);
     }
@@ -34,8 +32,9 @@ public class StudentController {
     public Optional<Student> getStudentByPath(@RequestParam Long id){
         return this.studentRepository.findById(id);
     }
+
     @PostMapping(path = "/createP")             //to post entity being private using DTO
-    public ResponseEntity<StudentDTO> Create(@RequestBody Student student){
+    public ResponseEntity<StudentDTO> CreateWithDto(@RequestBody Student student){
         studentRepository.save(student);
         StudentDTO studentDTO = new StudentDTO();
         BeanUtils.copyProperties(student,studentDTO);
@@ -43,16 +42,19 @@ public class StudentController {
     }
 
     @PostMapping(path = "/createNP")        //to post entity without being private without using DTO
-    public void registerNewStudent(@RequestBody Student student){
+    public void CreateWithoutDto(@RequestBody Student student){
         studentRepository.save(student);
     }
 
 
-    //    @DeleteMapping
-//    public void deleteStudent(Student student){
-//        studentRepository.delete(student);
-//    }
+    @DeleteMapping(path = "/deleteAll")        //to delete all entity
+    public void deleteAllStudent(Student student){ studentRepository.deleteAll(); }
 
+    @DeleteMapping(path = "/deleteByParam/{id}")   //to delete id entity by choosing Param
+    public void deleteStudentByParam (@PathVariable Long id){ this.studentRepository.deleteById(id); }
+
+    @DeleteMapping(path = "/deleteByPath")   //to delete entity by choosing path
+    public void deleteStudentByPath(@RequestParam Long id){ this.studentRepository.deleteById(id); }
 
 //    @PostMapping(path = "/createP")             //to post entity being private using DTO
 //    public ResponseEntity<StudentDTO> Create(@RequestBody Student student){
