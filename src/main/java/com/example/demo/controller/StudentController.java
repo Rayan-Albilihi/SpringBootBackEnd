00d46d18f410edb18176f026425e590713870dde
RyanAlbilihi <78346329+RyanAlbilihi@users.noreply.github.com>
2022-07-17 14:52:28 +0300

    Add (Update method) and move the "methods" from StudentController to StudentService

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,9 +1,8 @@
 package com.example.demo.controller;
 
 import com.example.demo.DTO.StudentDTO;
-import com.example.demo.repository.StudentRepository;
 import com.example.demo.entitiy.Student;
-import org.springframework.beans.BeanUtils;
+import com.example.demo.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,47 +12,60 @@ import java.util.Optional;
 @RequestMapping(path = "api/student")
 public class StudentController {
 
-    private final StudentRepository studentRepository;
+    private final StudentService studentService;
 
 
     @Autowired
-    public StudentController(StudentRepository studentRepository) {this.studentRepository = studentRepository;}
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
 
     @GetMapping(path = "/getAll")                   //return all entity in normal way
-    public ResponseEntity getAllStudents(){return  ResponseEntity.ok(this.studentRepository.findAll());}
+    public ResponseEntity getAll(){
+        return studentService.getAllStudents();
+    }
 
     @GetMapping(path = "/getParam/{id}")         //return specific id entity by using @PathVariable
-    public Optional<Student> getStudentByParam(@PathVariable Long id){
-        return this.studentRepository.findById(id);
+    public Optional<Student> getByParam(@PathVariable Long id){
+        return studentService.getStudentByParam(id);
     }
 
     @GetMapping(path = "/getPath")              //return specific Param by using @RequestParam
-    public Optional<Student> getStudentByPath(@RequestParam Long id){
-        return this.studentRepository.findById(id);
+    public Optional<Student> getByPath(@RequestParam Long id){
+        return this.studentService.getStudentByPath(id);
     }
 
     @PostMapping(path = "/createP")             //to post entity being private using DTO
-    public ResponseEntity<StudentDTO> CreateWithDto(@RequestBody Student student){
-        studentRepository.save(student);
-        StudentDTO studentDTO = new StudentDTO();
-        BeanUtils.copyProperties(student,studentDTO);
-        return  ResponseEntity.ok(studentDTO);
+    public ResponseEntity<StudentDTO> SaveWithDto(@RequestBody Student student){
+        return  studentService.CreateWithDto(student);
     }
 
     @PostMapping(path = "/createNP")        //to post entity without being private without using DTO
-    public void CreateWithoutDto(@RequestBody Student student){
-        studentRepository.save(student);
+    public void SaveWithoutDto(@RequestBody Student student){
+        studentService.CreateWithoutDto(student);
     }
 
 
     @DeleteMapping(path = "/deleteAll")        //to delete all entity
-    public void deleteAllStudent(){ studentRepository.deleteAll(); }
+    public String deleteAll(){
+        return studentService.deleteAllStudent();
+    }
 
     @DeleteMapping(path = "/deleteByParam/{id}")   //to delete id entity by choosing Param
-    public void deleteStudentByParam (@PathVariable Long id){ this.studentRepository.deleteById(id); }
+    public String deleteByParam (@PathVariable Long id){
+        return studentService.deleteStudentByParam(id);
+    }
 
     @DeleteMapping(path = "/deleteByPath")   //to delete entity by choosing path
-    public void deleteStudentByPath(@RequestParam Long id){ this.studentRepository.deleteById(id); }
+    public String deleteByPath(@RequestParam Long id){
+        return studentService.deleteStudentByPath(id);
+    }
+
+    @PutMapping(path = "/update")
+    public Student update(@RequestBody Student student){
+        return studentService.updateStudent(student);
+    }
+}
 
 //    @PostMapping(path = "/createP")             //to post entity being private using DTO
 //    public ResponseEntity<StudentDTO> Create(@RequestBody Student student){
@@ -63,5 +75,3 @@ public class StudentController {
 //            BeanUtils.copyProperties(student,studentDTO);
 //            return  ResponseEntity.ok(studentDTO);
 //        }else return ResponseEntity.notFound().build();}
-
-}
